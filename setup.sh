ca6fd11a76a05d454f612c551487370650341143
Meron Ogbai <okbaymeron@gmail.com>
2021-12-05 01:12:33 +0300

    Remove unnecessary stuff

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 
 RELATIVE_PATH=`dirname "$0"`
 ABSOLUTE_PATH=`(cd $RELATIVE_PATH && pwd)`
-ln -s $ABSOLUTE_PATH/.asdfrc ~/.asdfrc
-ln -s $ABSOLUTE_PATH/.default-gems ~/.default-gems
-ln -s $ABSOLUTE_PATH/.default-npm-packages ~/.default-npm-packages
 ln -s $ABSOLUTE_PATH/.gitconfig ~/.gitconfig
 ln -s $ABSOLUTE_PATH/.gitignore_global ~/.gitignore_global
 ln -s $ABSOLUTE_PATH/.p10k.zsh ~/.p10k.zsh
