2ee99e6963341ef61c529173e6ec6bbb679a2ecb
Meron Ogbai <okbaymeron@gmail.com>
2022-02-12 07:26:50 +0300

    Overwrite existing configs

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" &> /dev/null &&  pwd -P )"
 
-ln -s $SCRIPT_PATH/.gitconfig ~/.gitconfig
-ln -s $SCRIPT_PATH/.gitignore_global ~/.gitignore_global
-ln -s $SCRIPT_PATH/.p10k.zsh ~/.p10k.zsh
-ln -s $SCRIPT_PATH/.vimrc ~/.vimrc
-ln -s $SCRIPT_PATH/.zshrc ~/.zshrc
+ln -sf $SCRIPT_PATH/.gitconfig ~/.gitconfig
+ln -sf $SCRIPT_PATH/.gitignore_global ~/.gitignore_global
+ln -sf $SCRIPT_PATH/.p10k.zsh ~/.p10k.zsh
+ln -sf $SCRIPT_PATH/.vimrc ~/.vimrc
+ln -sf $SCRIPT_PATH/.zshrc ~/.zshrc
