f772a8ead754bf77cba82e509dc9b0a2d4ca5be5
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2022-07-24 19:43:56 +0300

    Lint sh script using shellcheck

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-SCRIPT_PATH="$( cd -- "$(dirname "$0")" &> /dev/null &&  pwd -P )"
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" > /dev/null &&  pwd -P )" 2>&1
 
-ln -sf $SCRIPT_PATH/.gitconfig ~/.gitconfig
-ln -sf $SCRIPT_PATH/.gitignore_global ~/.gitignore_global
-ln -sf $SCRIPT_PATH/.p10k.zsh ~/.p10k.zsh
-ln -sf $SCRIPT_PATH/.vimrc ~/.vimrc
-ln -sf $SCRIPT_PATH/.zshrc ~/.zshrc
+ln -sf "$SCRIPT_PATH"/.gitconfig ~/.gitconfig
+ln -sf "$SCRIPT_PATH"/.gitignore_global ~/.gitignore_global
+ln -sf "$SCRIPT_PATH"/.p10k.zsh ~/.p10k.zsh
+ln -sf "$SCRIPT_PATH"/.vimrc ~/.vimrc
+ln -sf "$SCRIPT_PATH"/.zshrc ~/.zshrc
+
