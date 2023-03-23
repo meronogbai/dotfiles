5f51b9b0262b96724e7daaf8f8a3e6b05a3a90b8
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-03-24 02:30:43 +0300

    Update nvim-tree setup

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,29 +1,6 @@
-local nvim_tree = require("nvim-tree")
+local nvim_tree = require('nvim-tree')
 local api = require('nvim-tree.api')
 
-local function on_attach(bufnr)
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+nvim_tree.setup()
 
-  api.config.mappings.default_on_attach(bufnr)
-
-  vim.keymap.set('n', '<leader>e', function()
-    api.tree.toggle(true)
-  end)
-
-  vim.keymap.set('n', 'l', api.node.open.edit)
-  vim.keymap.set('n', 'h', api.node.navigate.parent_close)
-end
-
-nvim_tree.setup {
-    on_attach = on_attach
-}
-
-
-local function open_nvim_tree()
-  -- always open the tree
-  api.tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
