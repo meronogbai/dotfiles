457f78878187350c57ec076434bc6f080c46bb5d
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-04-08 14:25:37 +0300

    Enable copilot for markdown files

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -1,3 +1,6 @@
 -- map <C-j> to accept copilot suggestion
 vim.cmd [[imap <silent><script><expr> <C-j> copilot#Accept("\<CR>")]]
 vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+  markdown = true,
+}
