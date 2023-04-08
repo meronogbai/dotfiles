5d689ac8dd6dcc1cd827bb7bb0cbaf44286e9afb
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-04-08 14:19:42 +0300

    Remove .vimrc

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" > /dev/null &&  pwd -P )" 2>&1
 ln -sf "$SCRIPT_PATH"/.gitconfig ~/.gitconfig
 ln -sf "$SCRIPT_PATH"/.gitignore_global ~/.gitignore_global
 ln -sf "$SCRIPT_PATH"/.p10k.zsh ~/.p10k.zsh
-ln -sf "$SCRIPT_PATH"/.vimrc ~/.vimrc
 ln -sf "$SCRIPT_PATH"/.zshrc ~/.zshrc
 ln -sf "$SCRIPT_PATH"/.config/nvim ~/.config/nvim
 
