dbca1e31c40dc655663dd5715cf430591687e8f3
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-05-16 17:18:40 +0300

    Update telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -5,10 +5,17 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles)
+vim.keymap.set('n', '<leader>ft', builtin.git_status)
+vim.keymap.set('n', '<leader>fs', builtin.symbols)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 vim.keymap.set('n', '<leader>fp', telescope.extensions.project.project)
 
 telescope.setup {
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  },
   extensions = {
     project = {
       sync_with_nvim_tree = true,
