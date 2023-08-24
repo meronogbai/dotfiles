e27b883eb33d49f25d2d62caeb18178a494c9a7b
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-08-24 08:58:27 +0300

    Hookup trouble and telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+local trouble = require("trouble.providers.telescope")
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep)
@@ -23,7 +25,11 @@ telescope.setup {
   },
   defaults = {
     file_ignore_patterns = { "^node_modules/", "^.git/" },
-    prompt_prefix = "🔍"
+    prompt_prefix = "🔍",
+    mappings = {
+      i = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
   },
   extensions = {
     project = {
