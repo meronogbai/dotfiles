71a607b62dab9a39d9dc0dea7fe723a3de9fd9bd
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-10-24 05:40:21 +0300

    Improve go to pr keymap

--- .config/nvim/after/plugin/go-to-pr.lua
+++ .config/nvim/after/plugin/go-to-pr.lua
@@ -5,5 +5,19 @@ vim.keymap.set('n', '<leader>pr', function()
   local fileName = vim.fn.expand('%')
   local commitHashCommand = 'git blame -L' .. lineNum .. ',' .. lineNum .. ' ' .. fileName .. " | awk '{print $1}'"
 
-  vim.fn.system('gh search prs --web' .. ' `' .. commitHashCommand .. '`')
+  local command = 'gh search prs' ..
+      ' `' .. commitHashCommand .. '`' .. " | awk '{print $2}' | xargs -I _ gh pr view --web _"
+
+  -- Execute the command and capture the output
+  local result = vim.fn.system(command)
+
+  -- Check the exit code to determine success or failure
+  if vim.v.shell_error == 0 then
+    -- Log success
+    print("Successfully found PR!")
+  else
+    -- Log failure and show the error message
+    print("Failed to find PR!")
+    print("Error: " .. result)
+  end
 end, { desc = 'Search for PR in github' })
