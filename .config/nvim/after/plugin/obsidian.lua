4a280c211e07f8bf34e724999af7b51389211ff5
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2023-11-23 10:05:00 +0300

    Fix obsidian rename

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -3,6 +3,6 @@ require("obsidian").setup({
   dir = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes",
   mappings = {
     -- use gk instead of gk because i need gf to format files
-    ["gk"] = require("obsidian.mapping").gf_passthrough(),
+    ["gk"] = require("obsidian.mappings").gf_passthrough(),
   }
 })
