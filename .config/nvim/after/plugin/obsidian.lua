f8b2580cdec7a0e08897610bf92c0a9e728bfd6b
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-01-06 04:55:19 +0300

    chore: handle obsidian.nvim update

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -1,8 +1,11 @@
+-- @see https://github.com/epwalsh/obsidian.nvim/issues/286#issuecomment-1877391540
+vim.opt_local.conceallevel = 2
+
 require("obsidian").setup({
   -- storing these notes in icloud
   dir = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes",
-  mappings = {
-    -- use gk instead of gk because i need gf to format files
-    ["gk"] = require("obsidian.mappings").gf_passthrough(),
-  }
+  -- mappings = {
+  --   -- use gk instead of gk because i need gf to format files
+  --   ["gk"] = require("obsidian.mappings").gf_passthrough(),
+  -- }
 })
