9419ef22224fb3f9aeb21f36d232ed6dabbd066f
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-01-07 15:52:06 +0300

    fix: obsidian mapping

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -4,8 +4,15 @@ vim.opt_local.conceallevel = 2
 require("obsidian").setup({
   -- storing these notes in icloud
   dir = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes",
-  -- mappings = {
-  --   -- use gk instead of gk because i need gf to format files
-  --   ["gk"] = require("obsidian.mappings").gf_passthrough(),
-  -- }
+  mappings = {
+    -- use gk instead of gk because i need gf to format files
+    ["gk"] = require("obsidian.mappings").gf_passthrough(),
+    -- Toggle check-boxes.
+    ["<leader>ch"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+  }
 })
