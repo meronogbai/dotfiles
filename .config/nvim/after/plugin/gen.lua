5ae24571ec7d76bc291a5fd73981483474ae1544
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-01-08 06:06:52 +0300

    chore: don't show prompt

--- .config/nvim/after/plugin/gen.lua
+++ .config/nvim/after/plugin/gen.lua
@@ -2,7 +2,6 @@ require('gen').setup(
   {
     model = "mistral",      -- The default model to use.
     display_mode = "split", -- The display mode. Can be "float" or "split".
-    show_prompt = true,     -- Shows the Prompt submitted to Ollama.
     no_auto_close = true,   -- Never closes the window automatically.
   }
 )
