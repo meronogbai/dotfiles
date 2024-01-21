046422ba2d43184278c1bd21bf8e1b341d818308
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-01-21 12:57:54 +0300

    chore: codellama as default

--- .config/nvim/after/plugin/gen.lua
+++ .config/nvim/after/plugin/gen.lua
@@ -1,6 +1,6 @@
 require('gen').setup(
   {
-    model = "mistral",      -- The default model to use.
+    model = "codellama",      -- The default model to use.
     display_mode = "split", -- The display mode. Can be "float" or "split".
     no_auto_close = true,   -- Never closes the window automatically.
   }
