057d58553c29247bf89d15055563180cfb4700ac
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-05-20 11:55:35 +0300

    fix: handle ts auto tag breaking change

--- .config/nvim/lua/meronogbai/plugins/treesitter.lua
+++ .config/nvim/lua/meronogbai/plugins/treesitter.lua
@@ -3,21 +3,36 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter.configs').setup {
+      require('nvim-treesitter.configs').setup({
         ensure_installed = { "rust", "tsx", "lua", "json", "css", "markdown", "markdown_inline" },
         auto_install = true,
         highlight = {
           enable = true,
           additional_vim_regex_highlighting = { 'markdown' }
         },
-        autotag = {
-          enable = true,
-          -- @see https://github.com/windwp/nvim-ts-autotag/issues/125
-          enable_close_on_slash = false,
-        },
-      }
+      })
     end,
-    dependencies = { 'windwp/nvim-ts-autotag' }
   },
   { 'nvim-treesitter/nvim-treesitter-context' },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          -- Defaults
+          enable_close = true,          -- Auto close tags
+          enable_rename = true,         -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        },
+        -- Also override individual filetype configs, these take priority.
+        -- Empty by default, useful if one of the "opts" global settings
+        -- doesn't work well in a specific filetype
+        per_filetype = {
+          ["html"] = {
+            enable_close = false
+          }
+        }
+      })
+    end,
+  }
 }
