7eb714f60e99c22ac1591a6787414bb86f44d581
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-08-13 12:47:09 +0300

    chore: cleanup markdown

--- .config/nvim/lua/meronogbai/plugins/init.lua
+++ .config/nvim/lua/meronogbai/plugins/init.lua
@@ -10,10 +10,4 @@ return {
 
   -- Multi-cursor support with <C-n>
   'mg979/vim-visual-multi',
-
-  -- Markdown preview
-  {
-    "iamcco/markdown-preview.nvim",
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
 }
---
+++ .config/nvim/lua/meronogbai/plugins/markdown.lua
@@ -0,0 +1,5 @@
+-- Markdown preview
+return {
+  "iamcco/markdown-preview.nvim",
+  build = function() vim.fn["mkdp#util#install"]() end,
+}
