1de4b9ecd97eb77b1c1ea8cd77d8e7dedd7cebeb
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2024-10-15 00:39:09 +0300

    chore: search commits only for now

--- .config/nvim/after/plugin/gh.lua
+++ .config/nvim/after/plugin/gh.lua
@@ -1,12 +1,11 @@
--- Searches for the PR that last updated the current line in github
+-- Opens the commit that last updated the current line in github
 -- Depends on the `gh` cli
 vim.keymap.set('n', '<leader>pr', function()
   local lineNum = vim.api.nvim__buf_stats(0).current_lnum
   local fileName = vim.fn.expand('%')
   local commitHashCommand = 'git blame -L' .. lineNum .. ',' .. lineNum .. ' ' .. fileName .. " | awk '{print $1}'"
 
-  local command = 'gh search prs' ..
-      ' `' .. commitHashCommand .. '`' .. " | awk '{print $2}' | xargs -I _ gh pr view --web _"
+  local command = 'gh browse `' .. commitHashCommand .. '`'
 
   -- Execute the command and capture the output
   local result = vim.fn.system(command)
@@ -14,16 +13,16 @@ vim.keymap.set('n', '<leader>pr', function()
   -- Check the exit code to determine success or failure
   if vim.v.shell_error == 0 then
     -- Log success
-    print("Successfully found PR!")
+    print("Successfully opened commit in GitHub!")
   else
     -- Log failure and show the error message
-    print("Failed to find PR!")
+    print("Failed to open commit in GitHub!")
     print("Error: " .. result)
   end
-end, { desc = 'Search for PR in github' })
+end, { desc = 'Open commit in github' })
 
 vim.keymap.set('n', '<leader>rp', function()
-  local command = 'gh repo view --web'
+  local command = 'gh browse'
 
   -- Execute the command and capture the output
   local result = vim.fn.system(command)
