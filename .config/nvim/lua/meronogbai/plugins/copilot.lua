7c8e020e364deaf13f0b1829caecd37461451757
Meron Ogbai <22526062+meronogbai@users.noreply.github.com>
2025-02-03 10:53:08 +0800

    chore: remove avante and copilot

--- .config/nvim/lua/meronogbai/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
---  Make sure to login via Copilot auth in cmd mode
-return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  opts = {
-    suggestion = { enabled = true },
-  },
-}
--- .config/nvim/lua/meronogbai/plugins/markdown.lua
+++ .config/nvim/lua/meronogbai/plugins/markdown.lua
@@ -1,5 +1,13 @@
--- Markdown preview
 return {
-  "iamcco/markdown-preview.nvim",
-  build = function() vim.fn["mkdp#util#install"]() end,
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", },
+    },
+    ft = { "markdown", },
+  },
 }
